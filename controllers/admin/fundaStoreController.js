7aadcc221a151f4b367faa177d78b007ab7b4d31
IspahanRahman <ispahanrahman133@gmail.com>
2024-01-18 16:31:25 +0600

    admin panel emoji,personal adduser interface implemented

--- controllers/admin/fundaStoreController.js
+++ controllers/admin/fundaStoreController.js
@@ -43,11 +43,52 @@ const create_kick_pack  = async(req,res)=>{
     }
 }
 
+const emoji_list  = async(req,res)=>{
+    try{
+        return res.render('pages/funda_store/emoji_list');
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({message:"Internal Server Error"});
+    }
+}
+
+const add_emoji  = async(req,res)=>{
+    try{
+        return res.render('pages/funda_store/add_emoji');
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({message:"Internal Server Error"});
+    }
+}
+
+const personal_add_list  = async(req,res)=>{
+    try{
+        return res.render('pages/funda_store/personal_add_list');
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({message:"Internal Server Error"});
+    }
+}
+
+const create_personal_add  = async(req,res)=>{
+    try{
+        return res.render('pages/funda_store/create_personal_add');
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({message:"Internal Server Error"});
+    }
+}
+
 
 module.exports ={
     funda_store_list,
     membership_list,
     create_membership,
     kick_pack_list,
-    create_kick_pack
+    create_kick_pack,
+    emoji_list,
+    add_emoji,
+    personal_add_list,
+    create_personal_add
+    
 }
--- routes/admin/fundaStoreRoute.js
+++ routes/admin/fundaStoreRoute.js
@@ -1,6 +1,6 @@
 const fundaStoreRoute = require('express').Router();
 
-const { funda_store_list, membership_list, create_membership, kick_pack_list,create_kick_pack } = require('../../controllers/admin/fundaStoreController');
+const { funda_store_list, membership_list, create_membership, kick_pack_list,create_kick_pack, emoji_list, add_emoji, personal_add_list, create_personal_add } = require('../../controllers/admin/fundaStoreController');
 const adminAuth = require('../../middlewares/adminAuthMiddleware');
 
 fundaStoreRoute.get('/funda_store_list',adminAuth,funda_store_list);
@@ -8,6 +8,10 @@ fundaStoreRoute.get('/membership_list',adminAuth,membership_list);
 fundaStoreRoute.get('/create_membership',adminAuth,create_membership);
 fundaStoreRoute.get('/kick_pack_list',adminAuth,kick_pack_list);
 fundaStoreRoute.get('/create_kick_pack',adminAuth,create_kick_pack);
+fundaStoreRoute.get('/emoji_list',adminAuth,emoji_list);
+fundaStoreRoute.get('/add_emoji',adminAuth,add_emoji);
+fundaStoreRoute.get('/personal_add_list',adminAuth,personal_add_list);
+fundaStoreRoute.get('/create_personal_add',adminAuth,create_personal_add);
 
 
 module.exports = fundaStoreRoute;
